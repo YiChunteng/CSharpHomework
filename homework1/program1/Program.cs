58dc6d1e2666ac2cc5027612ad15b840f4c862e9
YiChunteng <2017302580160@whu.edu.cn>
2018-09-12 12:23:00 +0800

    homework1

---
+++ homework1/program1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("请输入两个数字：");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("输出结果：");
+            int c = a * b;
+            Console.Write(c);
+        }
+    }
+}
